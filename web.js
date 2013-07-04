69bbd8e9cc62a5f07e87a3bfdb1a18ad55e4acb6
Caleb Hyde <caleb.hyde@gmail.com>
2013-07-04 20:54:00 +0000

    Update web.js to pull from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var responseText = fs.readFileSync('index.html').toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(responseText);
 });
 
 var port = process.env.PORT || 5000;
